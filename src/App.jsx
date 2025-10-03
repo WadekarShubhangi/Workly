d9601c5de76562f0f431c9895359df981af6fb7c
WadekarShubhangi <shubhangiwadekar1096@gmail.com>
2025-10-03 20:23:46 +0530

    feat: dashboard cards html done.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import "./App.css";
 import LoginModel from "./components/LoginModel/LoginModel";
 import SignupModel from "./components/SignupModel/SignupModel";
 import SidebarNav from "./components/SidebarNav/SidebarNav";
-import SearchBox from "./components/Searchbox/SearchBox";
 import { useContext } from "react";
 import WorklyContext from "./contexts/worklyContext";
 import { Outlet, Link } from "react-router-dom";
@@ -17,23 +16,25 @@ function App() {
           <SidebarNav />
         </aside>
 
-        <section className="container my-3">
-          <span className="sidebar-hamburger">
-            <button
-              className={`btn btn-light btn-sidebar ${
-                closeSideBar ? "open" : ""
-              }`}
-              onClick={() => setCloseSideBar(!closeSideBar)}
-            >
-              {closeSideBar ? (
-                <i className="bi bi-x"></i>
-              ) : (
-                <i className="bi bi-list"></i>
-              )}
-            </button>
-          </span>
-          <SearchBox />
-          <Outlet />
+        <section className="container mx-4">
+          <div className="my-3">
+            <span className="sidebar-hamburger">
+              <button
+                className={`btn btn-light btn-sidebar ${
+                  closeSideBar ? "open" : ""
+                }`}
+                onClick={() => setCloseSideBar(!closeSideBar)}
+              >
+                {closeSideBar ? (
+                  <i className="bi bi-x"></i>
+                ) : (
+                  <i className="bi bi-list"></i>
+                )}
+              </button>
+            </span>
+
+            <Outlet />
+          </div>
           {/* Nested routes will render here */}
         </section>
       </div>
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,9 +1,107 @@
+import SearchBox from "../../components/Searchbox/SearchBox";
+import "./Dashboard.css"
 const Dashboard = () => {
-    return(
-        <>
-        Dashboard
-        </>
-    )
-}
+  return (
+    <>
+      <SearchBox />
+      <section className="my-5">
+        <h2>Projects</h2>
+         <div className="row">
+          <div className="col-md-4">
+            <div className="card card-elevated">
+              <div className="card-body">
+                <span class="badge bg-warning-subtle text-warning fw-bold fs-6">
+                  Warning
+                </span>
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="card card-elevated">
+              <div className="card-body">
+                <span class="badge bg-success-subtle text-success fw-bold fs-6">
+                  Warning
+                </span>
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+              </div>
+            </div>
+          </div>
+          {/* card-elevated */}
+          <div className="col-md-4">
+            <div className="card card-elevated">
+              <div className="card-body">
+                <span class="badge bg-success-subtle text-success fw-bold fs-6">
+                  Warning
+                </span>
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="my-5">
+        <h2>My Tasks</h2>
+        <div className="row">
+          <div className="col-md-4">
+            <div className="card card-elevated">
+              <div className="card-body">
+                <span class="badge bg-warning-subtle text-warning fw-bold fs-6">
+                  Warning
+                </span>
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="card card-elevated">
+              <div className="card-body">
+                <span class="badge bg-success-subtle text-success fw-bold fs-6">
+                  Warning
+                </span>
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+              </div>
+            </div>
+          </div>
+          {/* card-elevated */}
+          <div className="col-md-4">
+            <div className="card card-elevated">
+              <div className="card-body">
+                <span class="badge bg-success-subtle text-success fw-bold fs-6">
+                  Warning
+                </span>
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default Dashboard;
